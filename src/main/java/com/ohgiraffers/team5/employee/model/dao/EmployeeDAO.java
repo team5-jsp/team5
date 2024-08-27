96ab0c6f13764ce8f817a982aefef289b4e023ac
jieunjieune <jieune120@gmail.com>
2024-08-27 12:49:48 +0900

    select, delete

--- src/main/java/com/ohgiraffers/team5/employee/model/dao/EmployeeDAO.java
+++ src/main/java/com/ohgiraffers/team5/employee/model/dao/EmployeeDAO.java
@@ -2,11 +2,11 @@ package com.ohgiraffers.team5.employee.model.dao;
 
 import com.ohgiraffers.team5.employee.model.dto.EmployeeDTO;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 import java.util.Properties;
+
+import static com.ohgiraffers.team5.common.jdbc.JDBCTemplate.close;
 
 public class EmployeeDAO {
 
@@ -29,5 +29,27 @@ public class EmployeeDAO {
         String query = prop.getProperty("selectALlEmpList");
 
         return empList;
+    }
+
+    public int deleteEmp(Connection con, String empId) {
+        PreparedStatement pstmt = null;
+
+        int result = 0;
+
+        String query = prop.getProperty("deleteEmp");
+
+        try {
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, empId);
+
+            result = pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(pstmt);
+        }
+
+        return result;
     }
 }
--- src/main/webapp/WEB-INF/views/main/main.jsp
+++ src/main/webapp/WEB-INF/views/main/main.jsp
@@ -4,17 +4,28 @@
     <title>Title</title>
 </head>
 <body>
-
-    <hr>
     
-    
-    <hr>
-    
+    <h3>1. 전체 직원 정보 조회</h3>
+    <p>(재직중인 직원만 조회됩니다.)</p>
+    <button onclick="location.href='${pageContext.servletContext.contextPath}/employee/list'">전체 직원정보 조회하기</button>
     
     <hr>
     
     
     <hr>
+    
+    
+    <hr>
+    
+    
+    <hr>
+    
+    <h3>5. 직원 정보 삭제 </h3>
+    <p>(삭제하고자 하는 직원의 사번을 입력해주세요.)</p>
+    <form action="${ pageContext.servletContext.contextPath }/employee/delete" method="post">
+        사번 : <input type="text" name="empId">
+        <button type="submit">직원 정보 삭제</button>
+    </form>
 
 </body>
 </html>
